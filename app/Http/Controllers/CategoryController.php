75a4496dce4d8ed6bb1981bd1b61a4b1871b5f58
cy322666 <cy322666@gmail.com>
2022-04-08 18:25:48 +0300

    create models

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function list()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function list()
+    {
+
+    }
+}
