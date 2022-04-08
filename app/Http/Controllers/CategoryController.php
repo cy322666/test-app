a8cbcb1df2e4d937a529201cea8d427b1d12c368
cy322666 <cy322666@gmail.com>
2022-04-09 00:41:18 +0300

    build category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,32 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Exceptions\DeleteCategoryException;
+use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryCollection;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function create()
+    public function create(CreateCategoryRequest $request): CategoryResource
     {
+        $category = Category::query()->create($request->validated());
 
+        return new CategoryResource($category);
     }
 
-    public function delete()
+    /**
+     * @throws DeleteCategoryException
+     */
+    public function delete(Category $category)
     {
+        if ($category->products->count() == 0) {
 
+            $category->delete();
+        } else
+            throw new DeleteCategoryException(Category::DELETED_USE_MESSAGE);
     }
 
-    public function update()
+    public function update(UpdateCategoryRequest $request, Category $category): CategoryResource
     {
+        $category->fill($request->validated());
+        $category->save();
 
+        return new CategoryResource($category->refresh());
     }
 
-    public function get()
+    /**
+     * @param Category $category
+     * @return CategoryResource
+     */
+    public function get(Category $category): CategoryResource
     {
-
+        return new CategoryResource($category);
     }
 
-    public function list()
+    public function list(): CategoryCollection
     {
-
+        return new CategoryCollection(Category::query()->paginate());
     }
 }
