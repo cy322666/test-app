ec9caa5999f4929130ecb75bdae43dd3e93c3f35
cy322666 <cy322666@gmail.com>
2022-04-11 00:05:53 +0300

    build models Product and Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    const DELETED_USE_MESSAGE = 'Category used';
+
+    const MIN_COUNT_CATEGORY = 2;
+    const MAX_COUNT_CATEGORY = 10;
+
+    protected $fillable = [
+        'uuid',
+        'name',
+    ];
+
+    protected $hidden = [
+        'id',
+    ];
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::generateUuid();
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,58 @@
 
 namespace App\Models;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'price',
+        'is_publish',
+        'is_deleted',
+    ];
+
+    protected $hidden = [
+        'id',
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::generateUuid();
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    /**
+     * Открепляет текущие категории и устанавливает новые
+     *
+     * @param array $array_uuids Массив с uuids категорий
+     * @return void
+     */
+    public function attachCategories(array $array_uuids)
+    {
+        $this->categories()->detach();
+
+        foreach ($array_uuids as $category_uuid) {
+
+            $this->categories()->attach([
+
+                'uuid' => Category::query()
+                    ->where('uuid', $category_uuid)
+                    ->firstOrFail()
+                    ?->id,
+            ]);
+        }
+    }
 }
