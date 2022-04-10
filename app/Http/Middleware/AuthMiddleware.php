00b45539ac8f1d81be096d2cb226dca7faa4d0d8
cy322666 <cy322666@gmail.com>
2022-04-11 01:17:51 +0300

    create auth middleware

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next) : mixed
+    {
+        return $next($request);
+    }
+}
