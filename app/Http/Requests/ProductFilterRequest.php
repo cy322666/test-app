3573acb3ed482d69db874f07fa9027393c4ce3dc
cy322666 <cy322666@gmail.com>
2022-04-14 15:06:13 +0300

    add request for filter product

---
+++ app/Http/Requests/ProductFilterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductFilterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'category_uuid' => 'uuid|uuid:categories',
+            'is_publish'    => 'string|max:1',
+            'product_name'  => 'string|min:3',
+            'category_name' => 'string|min:3',
+            'price_at' => 'int|numeric',
+            'price_to' => 'int|numeric',
+        ];
+    }
+}
